40afcb110676049d18abcadcecc547e7f3289bb6
zhousiqi47 <1792807037@qq.com>
2019-09-08 17:00:51 +0800

    printnumberinword

---
+++ src/com/example/PrintnumberInword.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class PrintnumberInword {
+	public static void main(String[] args) {
+		int number = 0;
+		switch(number) {
+			case 1:
+				System.out.println("One");break;
+			case 2:
+				System.out.println("Two");break;
+			case 3:
+				System.out.println("Three");break;
+			case 4:
+				System.out.println("Four");break;
+			case 5:
+				System.out.println("Five");break;
+			case 6:
+				System.out.println("Six");break;
+			case 7:
+				System.out.println("Seven");break;
+			case 8:
+				System.out.println("Eight");break;
+			case 9:
+				System.out.println("Nine");break;
+			default:
+				System.out.println("Error input");break;
+		}
+	}
+}
