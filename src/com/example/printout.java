e08073186e77b7d26507e38da45ef2e4b650d303
zhousiqi47 <1792807037@qq.com>
2019-09-08 16:42:27 +0800

    printout

---
+++ src/com/example/printout.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class printout {
+	public static void main(String[] args) {
+		System.out.println("            '__'");
+		System.out.println("            (oo)");
+		System.out.println("    +========\\/ ");
+		System.out.println("  / || %%% ||");
+		System.out.println("*   ||-----||");
+		System.out.println("    \"\"     \"\" ");
+	}
+
+}
